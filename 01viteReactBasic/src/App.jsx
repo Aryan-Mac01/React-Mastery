280080d0d2d898ba11c5d7de7592c150003d823a
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-09 01:13:11 +0530

    clean the folder

---
+++ 01viteReactBasic/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+
+  return (
+    
+      <h1>Full of Reaction | AM</h1> // Vite is a bundler for react which means it loads faster and collect the vital packages in a bundle <npm create vite@latest>
+    
+  )
+}
+
+export default App
