5f617516126a7d7f92e69c4fc26e90bc336fa595
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-10 03:32:01 +0530

    useState hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,28 +1,44 @@
-
+import { useState } from "react";
 
 function App() {
-  
-  let Counter = 15
+  let [counter, setCounter] = useState(15); // kyuki counter ki value change honi hai isliye we will use 'let' instead of 'const'
+
+  /*  useState ->  jaise kisi cheez ko UI me change karna ho tb useState use karte, ye state change kardeta
+      isme 'counter' ek value hai jo ye return karega
+      'setCounter' ek function hai jo ye return karega
+  */
+
+  //let Counter = 15
 
   const addValue = () => {
-    console.log("clicked", Math.random());
-    Counter = Counter+1;
-  }
-
-  return (
-
+    counter = counter + 1;
+    if (counter <= 20) {
+      setCounter(counter); // wo function ko idhr call karke value print
+    }
+    
     
 
+    /* ye counter ki value log me toh change kardega but samne website ke UI pe wo 15 hi rahegi, usko change karne ke liye 
+    usko sambhalne ke liye hooks aaye*/
+  };
+
+  const decValue = () => {
+    counter = counter - 1;
+    if (counter >= 0) {
+      setCounter(counter);
+    }
+    
+  };
+
+  return (
     <>
       <h1>Time to React</h1>
-      <h2>Counter value: {Counter}</h2>
+      <h2>Counter value: {counter}</h2>
 
-      <button
-      onClick={addValue}
-      >Add value</button>
-      <button>Dec value</button>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={decValue}>Dec value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
