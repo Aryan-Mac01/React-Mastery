80f8230eec7197811509bc75309b71dfdf98f2c3
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-09 21:13:20 +0530

    custom react in vite

--- 01viteReactBasic/src/App.jsx
+++ 01viteReactBasic/src/App.jsx
@@ -3,11 +3,19 @@ import React from "./React";
 //Vite is a bundler for react which means it loads faster and collect the vital packages in a bundle <npm create vite@latest>
 
 function App() {
+
+  const username = "Aryan mishra"
+
   return (
     <>
       <h1>Full of Reaction | AM</h1>
+      <h1>{username}</h1>
       <React />
     </> //These are known as fragments and are used to wrap multiple components together so that they can be rendered together
+
+    //<h1>{username}</h1> this is known as expression and this is evaluated expression because we are using the final outcome of the js variable
+
+
   );
 }
 
--- 01viteReactBasic/src/main.jsx
+++ 01viteReactBasic/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 
+
+const reactElement = React.createElement( //creating custom react as per react's syntax
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    //reactElement //......it is an object that's why it is called like this
+    <App/>
+  
 )
