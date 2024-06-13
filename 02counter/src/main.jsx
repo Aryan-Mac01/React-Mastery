e1510e9bb53ca7115ca54f3256c2a41ec65c7d15
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-14 03:31:22 +0530

    reactFiberDocs

--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -7,4 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+) 
+
+//CreateRoot -> the createroot helps to create a DOM, it creates a DOM and then compare it with the original one and update only those things that are changes in UI.
