295c8063a9a72a0507f1f14d23082bff08d10b91
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-17 23:48:01 +0530

    intial commit

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("oliver")
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
