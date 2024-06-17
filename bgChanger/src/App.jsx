d34f665d06d473aab77f19a580a233ca492c0dcb
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-18 03:14:05 +0530

    bgColoe complete

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -4,12 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("oliver")
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      
-    </>
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >red</button>
+          <button
+          onClick={() => setColor("black")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "black"}}
+          >Black</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "Green"}}
+          >Green</button>
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "Blue"}}
+          >Blue</button>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
