4a2035918703c9aeab91e7cad2f5f574daeabe4b
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-22 03:43:05 +0530

    button hover

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -58,7 +58,7 @@ function App() {
         />
         <button 
         onClick={copyPasswordToClipboard}
-        className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+        className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-900">
           Copy
         </button>
       </div>
