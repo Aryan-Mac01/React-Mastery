285ac8c36ad48d89640388ec244d3f9999668686
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-30 10:25:36 +0530

    router initial

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,15 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
-
-function useCurrencyInfo(currency){
+function useCurrenyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)//API Call
+        .then((res)=>{res.json()}) //convert the string format into the JSON format
+        .then((res)=>{setData(res[currency])})
         console.log(data);
     }, [currency])
     console.log(data);
     return data
 }
 
-export default useCurrencyInfo;
+export default useCurrenyInfo //custom hooks return the data and we will catch the data using this line
