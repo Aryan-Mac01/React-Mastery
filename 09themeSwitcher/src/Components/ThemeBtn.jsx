ccc9eea588d06191a2b9c27bccfaf8f3dce0ee29
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-07-02 00:03:29 +0530

    darkModeStatus

--- 09themeSwitcher/src/Components/ThemeBtn.jsx
+++ 09themeSwitcher/src/Components/ThemeBtn.jsx
@@ -1,5 +1,19 @@
+import useTheme from "../Context/Theme";
+
 export default function ThemeBtn() {
     
+    const {themeMode, lightMode, darkMode} = useTheme()
+    const onChangeBtn = () => {
+        const darkModeStatus  = e.currentTarget.checked
+
+        if(darkModeStatus){
+            darkMode()
+        }
+        else{
+            lightMode()
+        }
+    }
+
 
     return (
         <label className="relative inline-flex items-center cursor-pointer">
@@ -7,6 +21,8 @@ export default function ThemeBtn() {
                 type="checkbox"
                 value=""
                 className="sr-only peer"
+                onChange={onChangeBtn}
+                checked = {themeMode==="dark"}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Toggle Theme</span>
