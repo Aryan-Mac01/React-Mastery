4b5563d98503d590ad1141382b55750c0a360441
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-07-01 17:22:36 +0530

    UserContextProvider

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
      <h1>Context</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
