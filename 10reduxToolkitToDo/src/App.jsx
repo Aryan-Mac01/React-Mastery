70758c25237db3e8d5459eaa9a16e0ec0c292f3d
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-07-04 16:52:04 +0530

    removeTodo debug

--- 10reduxToolkitToDo/src/App.jsx
+++ 10reduxToolkitToDo/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <h1>Learn about Redux Toolkit</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
--- 10reduxToolkitToDo/src/features/todo/todoSlice.js
+++ 10reduxToolkitToDo/src/features/todo/todoSlice.js
@@ -1,36 +1,28 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
-//nano id generate unique ids
+import {createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-  todos: [{ id: 1, text: "Hello World" }],
-};
+    todos: [{id: 1, text: "Hello world"}]
+}
+
+
 
 export const todoSlice = createSlice({
-  name: "todo",
-  initialState,
-  reducers: {
-    addTodo: (state, action) => {
-      const todo = {
-        id: nanoid(),
-        text: action.payload, //payload is an object that contains id, email, password, text etc
-      };
-      state.todos.push(todo); //state me todo push kardega
-    },
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+    }
+})
 
-    /*state variable give the access of the current state i.e. we created initialState and when we change it then the 'state' 
-        variable will give the access of that changed state. Action gives us the values like if we want to delete a todo
-        then action will provide us the id of that todo.
-        */
-    removeTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => {
-        todo.id !== action.payload;
-      });
-    },
-
-    
-  },
-});
-
-export const {addTodo, removeTodo} = todoSlice.actions //export all the reducers individually
+export const {addTodo, removeTodo} = todoSlice.actions
 
 export default todoSlice.reducer
