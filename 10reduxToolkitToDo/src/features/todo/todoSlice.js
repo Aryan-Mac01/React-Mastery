66bc8e73ecd83aaf56d3ba6e23b62b454b7d9595
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-07-04 15:23:47 +0530

    reducers

--- 10reduxToolkitToDo/src/features/todo/todoSlice.js
+++ 10reduxToolkitToDo/src/features/todo/todoSlice.js
@@ -12,14 +12,23 @@ export const todoSlice = createSlice({
     addTodo: (state, action) => {
       const todo = {
         id: nanoid(),
-        text: action.payload, //payload is an object that contains id, email, password, text etc 
+        text: action.payload, //payload is an object that contains id, email, password, text etc
       };
+      state.todos.push(todo); //state me todo push kardega
     },
 
     /*state variable give the access of the current state i.e. we created initialState and when we change it then the 'state' 
         variable will give the access of that changed state. Action gives us the values like if we want to delete a todo
         then action will provide us the id of that todo.
         */
-    removeTodo: () => {},
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => {
+        todo.id !== action.payload;
+      });
+    },
+
+    
   },
 });
+
+export const {addTodo, removeTodo} = todoSlice.actions
