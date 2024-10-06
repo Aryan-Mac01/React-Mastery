cc98c272060d35674235f31a9292160b9a7d35b6
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-10-07 01:41:13 +0530

    backend final

--- Connection/backend/server.js
+++ Connection/backend/server.js
@@ -1,3 +1,41 @@
 import express from "express"
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Go to joke');
+});
+
+//get a list of 5 jokes 
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "JOKE 1",
+            Content: "This is joke-1"
+        },
+        {
+            id: 2,
+            title: "JOKE 2",
+            Content: "This is joke-2"
+        },
+        {
+            id: 3,
+            title: "JOKE 3",
+            Content: "This is joke-3"
+        },
+        {
+            id: 4,
+            title: "JOKE 4",
+            Content: "This is joke-4"
+        },
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+})
